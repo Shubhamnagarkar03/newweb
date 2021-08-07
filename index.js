729012091cb0d21f0d3799d1d3dbb05512766d71
Shubham Nagarkar <78836996+Shubhamnagarkar03@users.noreply.github.com>
2021-08-07 19:32:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function darkTheme() {
+  var page = $("body");
+  var myImage = $("#me");
+  var button = $(".btn-outline-dark")
+  page.toggleClass("invertpage");
+  myImage.toggleClass("keepimgstill");
+  
+
+};
